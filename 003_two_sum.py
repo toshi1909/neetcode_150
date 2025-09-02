c7018606ef434861dc1060949f01b7a5cdc5c47a
toshi1909 <toshipurohit19@gmail.com>
2025-09-02 10:53:36 +0530

    two_sum solution 2

--- 003_two_sum.py
+++ 003_two_sum.py
@@ -11,6 +11,17 @@ class Solution:
         else:
             return None
 
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, value in enumerate(nums):
+            remain = target - nums[i]
+            if (remain in num_dict) and (i != num_dict[remain]):
+                return [num_dict[remain], i]
+            else:
+                num_dict[nums[i]] = i
+        else:
+            return None
+
 def main():
     sol = Solution()
     s = [1,2,3,4,1]

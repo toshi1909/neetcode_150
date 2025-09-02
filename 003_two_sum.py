3dd70573da1e4a38f1a1a4ab625436c823c1df71
toshi1909 <toshipurohit19@gmail.com>
2025-09-02 10:41:12 +0530

    two_sum

---
+++ 003_two_sum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {value: index for index, value in enumerate(nums)}
+        # num_dict = {value: index for index, value in enumerate(x)}
+        for i, value in enumerate(nums):
+            remain = target - nums[i]
+            if (remain in num_dict) and (i!=num_dict[remain]):
+                return [i, num_dict[remain]]
+        else:
+            return None
+
+def main():
+    sol = Solution()
+    s = [1,2,3,4,1]
+    t = 7
+    print(sol.twoSum(s,t))
+
+if __name__ == "__main__":
+    main()
